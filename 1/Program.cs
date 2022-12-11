5862112318a48e2282a2c4dc64aea1622bf3e7e8
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-11 20:14:00 +0300

    Programm 1

---
+++ 1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+            string chislo = Console.ReadLine();
+            if (chislo.Length == 5)
+            {
+           
+
+                if (chislo[0] == chislo[4] && chislo[1] == chislo[3])
+                {
+                    Console.WriteLine("Ваше число: " +  chislo + " = палиндром.");
+                }
+                else Console.WriteLine("Ваше число: "+ chislo + " не палиндром.");
+            }
+            else Console.WriteLine("Пожалуйста, введите пятизначное число");
+
+            Console.ReadLine();
+
