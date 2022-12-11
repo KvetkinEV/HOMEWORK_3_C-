b3dddb4d369b273a6385a9510d33c3cf5909ae39
WIN-H1HBIIFATSD <kvetkinevgenij@gmail.com>
2022-12-11 20:21:18 +0300

    Programm 3

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+            int cube = Convert.ToInt32(Console.ReadLine());
+
+            void Cube(int[] Caube)
+            {
+                int schetchik = 0;
+                int length = Caube.Length;
+                while (schetchik < length)
+                {
+                    Caube[schetchik] = Convert.ToInt32(Math.Pow(schetchik, 3));
+                    schetchik++;
+                }
+            }
+
+            void Vivod(int[] coll)
+            {
+                int count = coll.Length;
+                int index = 0;
+                while (index < count)
+                {
+                    Console.Write(coll[index] + " ");
+                    index++;
+                }
+            }
+
+            int[] massivChisel = new int[cube + 1];
+            Cube(massivChisel);
+            Vivod(massivChisel);
+            Console.ReadLine();  
+
